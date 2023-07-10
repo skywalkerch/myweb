5b5c60239b3664a88c7c93a30b9c78aeda79b3a9
skywalkerch <skywalkerch@outlook.com>
2023-07-10 08:12:01 +0800

    修改网页url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   favicon: 'img/favicon.svg',
   
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://skywalkerch.vercel.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
