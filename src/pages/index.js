a1aba786547259f5134a9e9d198a7bb96356ad66
skywalkerch <skywalkerch@outlook.com>
2023-07-31 16:12:20 +0800

    修改背景图

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,8 +12,9 @@ function HomepageHeader() {
   return (
 
   <header className={clsx('hero hero--primary', styles.heroBanner)}>
+   
     <div className='container'>
-  <h1 className="hero__title">{siteConfig.title}</h1>
+    <h1 className="hero__title">{siteConfig.title}</h1>
   <p className="hero__subtitle">{siteConfig.tagline}</p>
   <Link
   className="button button--secondary button--lg"
